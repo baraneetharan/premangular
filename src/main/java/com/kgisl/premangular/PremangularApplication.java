16a904f706c108580085eebb3db81b9f324bd37a
Baraneetharan <baraneetharan@live.com>
2023-04-17 15:00:55 +0530

    first commit

---
+++ src/main/java/com/kgisl/premangular/PremangularApplication.java
@@ -0,0 +1,13 @@
+package com.kgisl.premangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremangularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremangularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kgisl/premangular/PremangularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgisl.premangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremangularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
